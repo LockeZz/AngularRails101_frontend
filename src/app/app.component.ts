0f99482bec024c04c61dfbe2c10367d82bc6ae18
LockeZz <LockeZz.PHF@gmail.com>
2021-04-05 22:21:53 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'home-library';
+  title = 'APP WORKS!';
+  books;
+
+  constructor(private http: Http) {
+    http
+      .get('http://localhost:3000/books.json')
+      .subscribe((res) => (this.books = res.json()));
+  }
 }
